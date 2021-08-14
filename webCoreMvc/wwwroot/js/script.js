d6f83b9728157173af0cdf7fc602cb6a217b2d36
Omar <omerq779@gmail.com>
2021-08-14 23:54:17 +0400

    created main and layout page

---
+++ webCoreMvc/wwwroot/js/script.js
@@ -0,0 +1,131 @@
+function fatima(){
+    document.getElementById("h3").innerText="Fatima Asrafy standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original."
+
+    document.getElementById("id1").innerText="FATIMA ASRAFY "
+    
+    document.getElementById("fatima").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("aklima").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("aklima").style.filter="  contrast(100%)"
+    document.getElementById("john").style.filter="  contrast(100%)"
+    document.getElementById("fatimama").style.filter="  contrast(100%)"
+    document.getElementById("johns").style.filter="  contrast(100%)"
+    document.getElementById("jannat").style.filter="  contrast(100%)"
+    document.getElementById("amar").style.filter="  contrast(100%)"
+    document.getElementById("cummins").style.filter="  contrast(100%)"
+
+    
+
+
+
+}
+function jannat(){
+    document.getElementById("h3").innerText="Jannat Tumpa The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original."
+
+    document.getElementById("id1").innerText="JANNAT TUMPA "
+    document.getElementById("jannat").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("aklima").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("fatima").style.filter="  contrast(100%)"
+    document.getElementById("john").style.filter="  contrast(100%)"
+    document.getElementById("fatimama").style.filter="  contrast(100%)"
+    document.getElementById("johns").style.filter="  contrast(100%)"
+    document.getElementById("aklima").style.filter="  contrast(100%)"
+    document.getElementById("amar").style.filter="  contrast(100%)"
+    document.getElementById("cummins").style.filter="  contrast(100%)"
+
+
+}
+function johns(){
+    document.getElementById("h3").innerText="Standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original since the 1500s."
+
+    document.getElementById("id1").innerText="JOHNS DUE "
+    document.getElementById("johns").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("aklima").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("fatima").style.filter="  contrast(100%)"
+    document.getElementById("john").style.filter="  contrast(100%)"
+    document.getElementById("fatimama").style.filter="  contrast(100%)"
+    document.getElementById("aklima").style.filter="  contrast(100%)"
+    document.getElementById("jannat").style.filter="  contrast(100%)"
+    document.getElementById("amar").style.filter="  contrast(100%)"
+    document.getElementById("cummins").style.filter="  contrast(100%)"
+
+
+}
+function john(){
+    document.getElementById("h3").innerText="John Smith The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original."
+
+    document.getElementById("id1").innerText=" JOHN SMITH"
+    document.getElementById("john").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("aklima").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("fatima").style.filter="  contrast(100%)"
+    document.getElementById("aklima").style.filter="  contrast(100%)"
+    document.getElementById("fatimama").style.filter="  contrast(100%)"
+    document.getElementById("johns").style.filter="  contrast(100%)"
+    document.getElementById("jannat").style.filter="  contrast(100%)"
+    document.getElementById("amar").style.filter="  contrast(100%)"
+    document.getElementById("cummins").style.filter="  contrast(100%)"
+
+
+}
+function aklima(){
+    document.getElementById("h3").innerText="Ben Nurlan Gulu, 2017 senesinden buyana blog yazarlığı yapıyorum, ve işimi çok seviyorum."
+
+    document.getElementById("id1").innerText="Nurlan "
+    document.getElementById("aklima").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("fatima").style.filter="  contrast(100%)"
+    document.getElementById("john").style.filter="  contrast(100%)"
+    document.getElementById("fatimama").style.filter="  contrast(100%)"
+    document.getElementById("johns").style.filter="  contrast(100%)"
+    document.getElementById("jannat").style.filter="  contrast(100%)"
+    document.getElementById("amar").style.filter="  contrast(100%)"
+    document.getElementById("cummins").style.filter="  contrast(100%)"
+
+}
+function amar(){
+    document.getElementById("h3").innerText="Chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original original since the 1500s."
+
+    document.getElementById("id1").innerText="AMAR ORTHI  "
+    document.getElementById("amar").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("aklima").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("fatima").style.filter="  contrast(100%)"
+    document.getElementById("john").style.filter="  contrast(100%)"
+    document.getElementById("fatimama").style.filter="  contrast(100%)"
+    document.getElementById("johns").style.filter="  contrast(100%)"
+    document.getElementById("jannat").style.filter="  contrast(100%)"
+    document.getElementById("aklima").style.filter="  contrast(100%)"
+    document.getElementById("cummins").style.filter="  contrast(100%)"
+
+
+}
+function fatimama(){
+    document.getElementById("h3").innerText="The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original."
+
+    document.getElementById("id1").innerText="FATIMA MA "
+    document.getElementById("fatimama").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("aklima").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("fatima").style.filter="  contrast(100%)"
+    document.getElementById("john").style.filter="  contrast(100%)"
+    document.getElementById("aklima").style.filter="  contrast(100%)"
+    document.getElementById("johns").style.filter="  contrast(100%)"
+    document.getElementById("jannat").style.filter="  contrast(100%)"
+    document.getElementById("amar").style.filter="  contrast(100%)"
+    document.getElementById("cummins").style.filter="  contrast(100%)"
+
+
+}
+function cummins(){
+    document.getElementById("h3").innerText="Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum reproduced below original since the 1500s."
+
+    document.getElementById("id1").innerText="JON CUMMINS "
+
+    document.getElementById("cummins").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("aklima").style.filter="  contrast(100%) drop-shadow(8px 8px 10px red) invert(0%)"
+    document.getElementById("fatima").style.filter="  contrast(100%)"
+    document.getElementById("john").style.filter="  contrast(100%)"
+    document.getElementById("fatimama").style.filter="  contrast(100%)"
+    document.getElementById("johns").style.filter="  contrast(100%)"
+    document.getElementById("jannat").style.filter="  contrast(100%)"
+    document.getElementById("amar").style.filter="  contrast(100%)"
+    document.getElementById("aklima").style.filter="  contrast(100%)"
+
+}
+    
